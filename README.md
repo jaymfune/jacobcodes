424c94fdaf1ce6da26dd8dea194a7d7bc2b7727c
Jacob <jacobmfune2@gmail.com>
2022-07-16 12:33:04 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Jacob Mfune Portfolio Site
+
+This is portfolio site to showcase Jacob's abilities, skills and knowledge in HTML, SCSS and JavaScript as a self-taught fullstack developer looking to get hired.
+
+The completion of this project has taught me a few things these are some of the challenges and lessons I have learnt so far :
+* Accessibility features like using the aria-label attribute to label items on my site that do not have initial text like my mobile nav menu and social media icons.
+* Using JavaScript to make my mobile nav menu have a dynamic interaction with the users by opening and closing onclick where I used a eventListener to my nav menu button.
+* Simple CSS rules like scroll-behavior: smooth that solved a problem I had with having my nav links link to sections on the same page jump from one section to another while now there is a smooth scroll up/down from one section to another.
+
+
+
+## Acknowledgements
+ - [How to write a Good readme](https://bulldogjob.com/news/449-how-to-write-a-good-readme-for-your-github-project)
+ - [Sass, BEM, & Responsive Design](https://www.youtube.com/watch?v=jfMHA8SqUL4)
---
+++ app/js/script.js
@@ -0,0 +1,124 @@
+// Declaration of variables  
+const toggle = document.querySelector("#toggle");
+const navbar = document.querySelector(".navbar");
+const nav = document.querySelector(".navbar__wrapper");
+const links = document.querySelectorAll(".navbar__links li");
+const navHeight = nav.offsetHeight;
+const card = document.querySelector(".project-card");
+
+// On click add/remove class on nav
+toggle.addEventListener("click", function () {
+  this.classList.toggle("active");
+  nav.classList.toggle("open");
+});
+
+
+// On mobile check if nav has class open
+links.forEach(link => {
+  link.addEventListener("click", function () {
+    if (nav.classList.contains("open")) {
+      nav.classList.remove("open");
+      toggle.classList.remove("active");
+    }
+  })
+});
+
+// On scroll check nav height and add class when window.scrollY > nav
+window.addEventListener("scroll", function() {
+  if(Math.floor(this.scrollY) > 0){
+    navbar.classList.add("fixed");    
+  } else {
+    navbar.classList.remove("fixed");
+  }
+})
+
+// Invoke getData()
+getData();
+
+// Fetch data from project.json
+// function getData() {
+//   fetch("app/json/projects.json")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+
+//       data.map((element)=> {
+//         return card.innerHTML += `
+//           <article id="${element.id}" class="card">
+//             <div class="card__img">
+//               <figure>
+//                 <img src="${element.img}" alt="${element.imgAlt}">
+//               </figure>
+//             </div>
+
+//             <div class="card__description">
+//               <h3 class="title-line">${element.title}</h3>
+//               <ul>
+//                 ${element.techstack.map((element) => `<li>${element}</li>`).join('')}
+//               </ul>
+//               <p>${element.description}</p>
+//               <div class="btns">
+//                 <a href="${element.url}" target="_blank" rel="noopener noreferrer">Live Site</a>
+//                 ${(()=> {
+//                   if(element.githubUrl !== undefined){
+//                     return `<a href="${element.githubUrl}" target="_blank" rel="noopener noreferrer">Github</a>`
+//                   } else {
+//                     return ""
+//                   }
+//                 })()}
+//               </div>
+//             </div>
+//           </article>
+//         `
+//       });
+//     })
+//     .catch((error) => {
+//       console.error("Error:", error);
+//     });
+// }
+
+// Fetch data from project.json
+async function getData() {
+  // Make a fetch request to get data from project.json
+  const response = await fetch("app/json/projects.json")
+
+  // Check if response was successful or not using the .ok method
+  if (!response.ok) {
+		throw new Error(`${animeQuote.innerHTML = `HTTP error! status: ${response.status}`}`);
+	}
+
+  // Retrieve response as JSON using the json() function
+  const data = await response.json()
+
+  // Iterate through the response using the map function
+  data.map((element)=> {
+    // Use DOM to create new elements
+    return card.innerHTML += `
+      <article id="${element.id}" class="card">
+        <div class="card__img">
+          <figure>
+            <img src="${element.img}" alt="${element.imgAlt}">
+          </figure>
+        </div>
+
+        <div class="card__description">
+          <h3 class="title-line">${element.title}</h3>
+          <ul>
+            ${element.techstack.map((element) => `<li>${element}</li>`).join('')}
+          </ul>
+          <p>${element.description}</p>
+          <div class="btns">
+            <a href="${element.url}" target="_blank" rel="noopener noreferrer">Live Site</a>
+            ${(()=> {
+              if(element.githubUrl !== undefined){
+                return `<a href="${element.githubUrl}" target="_blank" rel="noopener noreferrer">Github</a>`
+              } else {
+                return ""
+              }
+            })()}
+          </div>
+        </div>
+      </article>
+    `
+  });  
+}
